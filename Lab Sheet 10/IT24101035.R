c4c7ef1d282f03910f59d9f69c2b4d4096ad750a
IT24101035 <IT24101035@my.sliit.lk>
2025-10-22 23:24:35 +0530

    Add files via upload

---
+++ Lab Sheet 10/IT24101035.R
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\asus\\Documents\\2 Year 1 Sem\\PS\\Lab Practical\\Lab 10")
+getwd()
+
+# Chi-Square Goodness-of-Fit Test for Snack Types
+
+
+snack_types <- c("A", "B", "C", "D")
+observed <- c(120, 95, 85, 100)
+
+
+expected_prob <- c(0.25, 0.25, 0.25, 0.25)
+
+
+test_result <- chisq.test(x = observed, p = expected_prob)
+
+
+print("Chi-Square Goodness-of-Fit Test Results:")
+print(test_result)
+
+
+print("Expected counts:")
+print(test_result$expected)
+
